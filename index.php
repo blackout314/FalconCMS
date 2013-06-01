db68bc59bc3b2e8de8f9dd026a378975dcf6e543
blackout <blackout@xps.grayhats.org>
2013-06-01 16:09:45 +0200

    pages template fix

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
  * carlo 'blackout' denaro - blackout@grayhats.org
  */
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 include 'etc/config.php';
 include 'lib/template/raintpl/rain.tpl.class.php';
